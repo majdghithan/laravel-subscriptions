ccf2aa9c964068bbf6e29284312b46064c106514
Ashraf Monshi <wh7r.com@gmail.com>
2022-04-04 01:48:24 +0300

    remove unique

--- src/Models/PlanFeature.php
+++ src/Models/PlanFeature.php
@@ -141,7 +141,8 @@ class PlanFeature extends Model implements Sortable
         $this->setTable(config('rinvex.subscriptions.tables.plan_features'));
         $this->mergeRules([
             'plan_id' => 'required|integer|exists:'.config('rinvex.subscriptions.tables.plans').',id',
-            'slug' => 'required|alpha_dash|max:150|unique:'.config('rinvex.subscriptions.tables.plan_features').',slug',
+            'slug' => 'required|alpha_dash|max:150',
+            //'slug' => 'required|alpha_dash|max:150|unique:'.config('rinvex.subscriptions.tables.plan_features').',slug',
             'name' => 'required|string|strip_tags|max:150',
             'description' => 'nullable|string|max:32768',
             'value' => 'required|string',
